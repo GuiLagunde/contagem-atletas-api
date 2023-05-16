bddbe4fd8ef2a18f0df219a4fefcca898ab83229
GuiLagunde <https://GuiLagunde@github.com>
2023-05-16 17:17:50 -0300

    inicial

---
+++ src/main/java/com/contagematletas/contagematletasapi/ContagematletasApiApplication.java
@@ -0,0 +1,13 @@
+package com.contagematletas.contagematletasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContagematletasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContagematletasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contagematletas/contagematletasapi/ContagematletasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contagematletas.contagematletasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContagematletasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
